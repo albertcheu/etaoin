55062dfbbd89e3cb959e007c4ea6a850cf7ea510
Albert Cheu <albert.cheu@gmail.com>
2014-06-25 10:26:08 -0400

    Made handlers.py to hold handleX(); sceneInput has a pentagon in the lower right

--- constants.py
+++ constants.py
@@ -7,3 +7,7 @@ TOP, RIGHT, BOTTOM, LEFT = 0,1,2,3
 DEFNS = {3:"triangle",4:"quadrilateral",5:"pentagon",6:"hexagon",8:"octagon"}
 REVDEFNS = {'triangle':3,'quadrilateral':4,'pentagon':5,'hexagon':6,'octagon':8}
 SNUM = {'one':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8}
+DNE = "That doesn't exist"
+THEPROB = "Be more specific or more general"
+YES = "Yes"
+NO = "No"
